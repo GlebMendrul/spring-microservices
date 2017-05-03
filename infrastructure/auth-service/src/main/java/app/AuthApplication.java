b6d8e686083fdbc4aed2a59bf149efe901dc21f3
gleb <mendrul.gleb@gmail.com>
2017-05-03 16:58:18 +0200

    removed unnecessary modules

--- infrastructure/auth-service/src/main/java/app/AuthApplication.java
+++ infrastructure/auth-service/src/main/java/app/AuthApplication.java
@@ -3,12 +3,14 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 /**
  * Created by gleb on 3/18/2017.
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AuthApplication {
 
     public static void main(String[] args) throws Exception {
--- infrastructure/hystrix-dashboard/src/main/java/app/HystrixDashboardApplication.java
+++ infrastructure/hystrix-dashboard/src/main/java/app/HystrixDashboardApplication.java
@@ -3,11 +3,13 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 /**
  * Created by gleb on 3/19/2017.
  */
 @SpringBootApplication
+@EnableTurbineStream
 @EnableHystrixDashboard
 public class HystrixDashboardApplication {
 
